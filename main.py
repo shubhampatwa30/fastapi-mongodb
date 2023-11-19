0463ffe99483367d8ebaa30052b6541d122fb7cf
shubhampatwa30 <shubham3001@gmail.com>
2023-11-19 16:45:33 -0600

    initial run completed

--- main.py
+++ main.py
@@ -1,19 +1,60 @@
 import asyncio
 import config
 from motor.motor_asyncio import AsyncIOMotorClient
+from bson.objectid import ObjectId
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
-async def ping_server():
-  # Replace the placeholder with your Atlas connection string
-  uri = f"mongodb://{config.username}:{config.password}@ac-t24yne5-shard-00-00.m4g5i3m.mongodb.net:27017,ac-t24yne5-shard-00-01.m4g5i3m.mongodb.net:27017,ac-t24yne5-shard-00-02.m4g5i3m.mongodb.net:27017/?ssl=true&replicaSet=atlas-vxt03s-shard-0&authSource=admin&retryWrites=true&w=majority"
+class Student(BaseModel):
+    name: str
+    age: int
 
-  # Create a new client and connect to the server
-  client = AsyncIOMotorClient(uri)
+app = FastAPI()
 
-  # Send a ping to confirm a successful connection
-  try:
-      await client.admin.command('ping')
-      print("Pinged your deployment. You successfully connected to MongoDB!")
-  except Exception as e:
-      print(e)
-      
-asyncio.run(ping_server())
+uri = config.uri
+client = AsyncIOMotorClient(uri)
+
+
+
+db = client["library"]
+
+students_collection = db.students
+books_collection = db.books
+books_lent = db.books_lent
+
+
+@app.get("/")
+async def root():
+    return {"message": "Heath check succeeded"}
+
+
+
+@app.post("/students/", response_description="Add new student",response_model=Student)
+async def insert_student(student: Student):
+    student = jsonable_encoder(student)
+    new_student = await students_collection.insert_one(student)
+    created_student = await students_collection.find_one({"_id": new_student.inserted_id})
+    return created_student
+
+
+@app.delete("/student/{student_name}", response_description="Delete a student")
+async def delete_student(student_name: str):
+    # document = {"name": student}
+    result = await students_collection.delete_one({ "name": student_name });
+    # print("result %s" % repr(result.inserted_id))
+    return {"student_deleted": student_name}
+
+@app.put("/students/{old_student_name}", response_description="Update a student")
+async def update_student(old_student_name: str, new_student_name: str):
+    old_document = await students_collection.find_one({"name": old_student_name})
+    _id = old_document["_id"]
+    result = await students_collection.replace_one({"_id": _id}, {"name": new_student_name})
+    return {"Student Updated: ": new_student_name}
+    
+@app.get("/students/{student_id}")
+async def get_student_by_id(student_id):
+    document = await students_collection.find_one({"_id": ObjectId(student_id)})
+    if document != None:
+        return {"Student": document['name']}
+    
--- readme.md
+++ readme.md
@@ -1,2 +1,36 @@
 Hello world!
-This is a project to learn about fastapi and mongoDB
+This is a project to learn about fastapi and mongoDB with simple project of library management system
+
+MongoDB <> MySQL
+Database <> Database
+Collections <> Tables
+Documents <> Rows/Records
+
+
+
+Tables:
+Student
+Books
+Library
+
+
+# Students collection
+
+id_, student name
+
+# Books collection
+
+id_ , book_name, author, published date, library_added_date
+
+# LentBooks collection
+
+id_ , book_id, student_id , lent date 
+
+# history collection
+
+id_, book_id, student_id, lent_date, return_date 
+
+
+
+
+
